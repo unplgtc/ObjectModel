3b2aa8d840dccded126be3ccb3ccbb540d75b0bb
Alex Guyot <ajguyot@icloud.com>
2019-12-20 00:17:58 -0700

    Remove ajv-keywords dep, add built-in 'is' keyword which checks both isPrototypeOf and instanceof, add new 'addKeyword()' function which will let users extend AJV with custom keywords, restructure expected argument format for addTypes function to take an Object instead of an Array

--- src/services/ajvService.js
+++ src/services/ajvService.js
@@ -3,24 +3,86 @@
 const Ajv = require('ajv');
 const ajv = new Ajv({ verbose: true });
 
-// Add the `instanceof` keyword to our ajv instance
-require('ajv-keywords')(ajv, 'instanceof');
+const TYPES = {
+	Object: Object,
+	Array: Array,
+	Function: Function,
+	Number: Number,
+	String: String,
+	Date: Date,
+	RegExp: RegExp,
+	Buffer: Buffer,
+	Promise: Promise
+}
 
-const ajvInstanceof = require('ajv-keywords/keywords/instanceof').definition;
+const isKeyword = {
+	compile: function(schema) {
+		if (typeof schema === 'string') {
+			const type = getType(schema);
+			return function(data) {
+				try {
+					return type.isPrototypeOf(data) || data instanceof type;
+				} catch (err) {
+					// Not the prototype, and cannot call instanceof on this Object
+					return false;
+				}
+			}
+		} else {
+			const types = schema.map(getType);
+			return function(data) {
+				for (const type of types) {
+					try {
+						if (type.isPrototypeOf(data) || data instanceof type) {
+							return true;
+						}
+					} catch (err) {}
+				}
+				return false;
+			}
+		}
+	},
+
+	metaSchema: {
+		anyOf: [
+			{ type: 'string' },
+			{
+				type: 'array',
+				items: { type: 'string' }
+			}
+		]
+	}
+}
+
+function getType(type) {
+	if (TYPES[type]) {
+		return TYPES[type];
+	}
+	throw new Error(`Invalid 'is' keyword type '${type}'`);
+}
+
+ajv.addKeyword('is', isKeyword);
 
 const ajvService = {
 	get ajv() {
 		return ajv;
 	},
 
+	addKeyword(keyword, validator) {
+		ajv.addKeyword(keyword, validator);
+	},
+
 	addTypes(types) {
-		for (const nameAndType of types) {
-			this.addType(...nameAndType);
+		for (const [name, type] of Object.entries(types)) {
+			this.addType(name, type);
 		}
 	},
 
 	addType(name, type) {
-		ajvInstanceof.CONSTRUCTORS[name] = type;
+		if (!TYPES[name]) {
+			TYPES[name] = type;
+		} else {
+			throw new Error(`Type '${name}' already added to AJV`);
+		}
 	}
 }
 
